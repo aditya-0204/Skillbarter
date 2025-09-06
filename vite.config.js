96f327626b6c40330a2a7a7627f75925aebefe58
Aditya Kumawat <adityakumawat2003@gmail.com>
2025-09-07 00:43:01 +0530

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // <-- important for Vercel
+  base: "/Skillbarter/", // 👈 this matches your repo name
 });
