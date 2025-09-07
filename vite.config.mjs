705aa278df8905bf9e81791a2816903294004111
Aditya Kumawat <adityakumawat2003@gmail.com>
2025-09-07 13:37:01 +0530

    Fix vite config for GitHub Pages

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // 👈 important for Vercel deployment
+  base: '/Skillbarter/', // 👈 repo name
 })
