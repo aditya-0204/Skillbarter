650a0f0195843c9a400bb95cd01dc855c1920513
Aditya Kumawat <adityakumawat2003@gmail.com>
2025-09-07 13:24:19 +0530

    delete

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/Skillbarter/", // 👈 this matches your repo name
-});
