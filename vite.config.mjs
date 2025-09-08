dcccc9cbd2a152294a8b8dfe1cd055ab3997eb39
Aditya Kumawat <adityakumawat2003@gmail.com>
2025-09-09 01:29:08 +0530

    updated mjs file

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/",
+export default defineConfig(({ command }) => {
+  const isBuild = command === 'build';
+
+  return {
+    plugins: [react()],
+    // Set the base path conditionally
+    base: isBuild ? "/Skillbarter/" : "/",
+  }
 })
