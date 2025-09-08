802b27f3d6f7bf36d0105fd41f52b497cda5e3f9
Aditya Kumawat <adityakumawat2003@gmail.com>
2025-09-09 01:54:56 +0530

    updated main.jsx

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   const isBuild = command === 'build';
-
   return {
     plugins: [react()],
-    // Set the base path conditionally
+    // This line makes your code work in both places
     base: isBuild ? "/Skillbarter/" : "/",
   }
 })
