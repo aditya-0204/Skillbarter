098b221dd0196ce870ae991fe638e7883fbc2c37
Aditya Kumawat <adityakumawat2003@gmail.com>
2025-09-09 18:30:53 +0530

    done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './firebase.js'; // Initializes Firebase
 import './index.css';
+import App from './App.jsx';
+import { AuthProvider } from './contexts/AuthContext.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>If you can see this, the test works.</h1>
+    {/* For Vercel, no 'basename' is needed */}
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
+
